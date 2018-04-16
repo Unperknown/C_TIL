1fc7fe05b2c507f35a7d388cb1455cb01214df48
unKNOWn <37747694+Unperknown@users.noreply.github.com>
2018-04-16 21:09:45 +0900

    Ant is working hardly again

--- C source/hardWorkingAnt.c
+++ C source/hardWorkingAnt.c
@@ -1,61 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-	
+     
 int main()
 {
-	int **hwA = malloc(sizeof(int *) * 10);
-	
-	for (int i = 0; i < 10; i++)
-		hwA[i] = malloc(sizeof(int) * 10);
-	
-	for (int i = 0; i < 10; i++)
-	{
-		for (int j = 0; j < 10; j++)
-		{
-			scanf("%d", &hwA[i][j]);
-		}
-	}
-	
-	int column = 1, row = 1;
-	
-	while (1)
-	{
-		if (hwA[column][row + 1] == 0)
-		{
-			hwA[column][row] = 9;
-			row++;
-		}
-		else if (hwA[column][row + 1] == 1 && hwA[column + 1][row] == 0)
-		{
-			hwA[column][row] = 9;
-			column++;
-		}
-		else if (hwA[column][row + 1] == 1 && hwA[column + 1][row] == 2)
-		{
-			hwA[column][row] = 9;
-			column++;
-			hwA[column][row] = 9;
-			break;
-		}
-		else
-			hwA[column][row] = 9;
-			break;
-	}
-	
-	for (int i = 0; i < 10; i++)
-	{
-		for (int j = 0; j < 10; j++)
-		{
-			printf("%d ", hwA[i][j]);
-		}
-		
-		printf("\n");
-	}
-	
-	for (int i = 0; i < 10; i++)
-		free(hwA[i]);
-	
-	free(hwA);
-	
-	return 0;
+    int **hwA = malloc(sizeof(int *) * 10);
+     
+    for (int i = 0; i < 10; i++)
+        hwA[i] = malloc(sizeof(int) * 10);
+     
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            scanf("%d", &hwA[i][j]);
+        }
+    }
+     
+    int column = 1, row = 1;
+     
+    while (1)
+    {
+        if (hwA[column][row + 1] == 0)
+        {
+            hwA[column][row] = 9;
+            row++;
+        }
+        else if (hwA[column][row + 1] == 1 && hwA[column + 1][row] == 0)
+        {
+            hwA[column][row] = 9;
+            column++;
+        }
+        else if (hwA[column][row + 1] == 1 && hwA[column + 1][row] == 2)
+        {
+            hwA[column][row] = 9;
+            column++;
+            break;
+        }
+        else
+            break;
+    }
+     
+    hwA[column][row] = 9;
+     
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d ", hwA[i][j]);
+        }
+         
+        printf("\n");
+    }
+     
+    for (int i = 0; i < 10; i++)
+        free(hwA[i]);
+     
+    free(hwA);
+     
+    return 0;
 }

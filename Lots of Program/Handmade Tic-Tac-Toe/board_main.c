8539b5719008c4c2271f2b2f0abfba0b86c25627
unKNOWn <wnsdud105@gmail.com>
2018-05-30 10:49:53 +0900

    Updated that.

--- Lots of Program/Handmade Tic-Tac-Toe/board_main.c
+++ Lots of Program/Handmade Tic-Tac-Toe/board_main.c
@@ -1,6 +1,9 @@
 #include "board.h"
 
 extern int status;
+static void playerSet();
+static void gameStart();
+static void gameResult();
 
 static char * s_get(char * name, int max);
 int main()
@@ -10,34 +13,10 @@ int main()
 	do
 	{
 		clean(board);
-		do
-		{
-			if (temp) printf("Enter different nickname.\n");
-			printf("Enter Player1's name.\n< ");
-			s_get(player1, 50);
-			printf("Enter Player2's name.\n< ");
-			s_get(player2, 50);
-		} while (temp = !strcmp(player1, player2));
-	
-		printf("Player1(O): %s VS Player2(X): %s\n", player1, player2);
-		show_board(board);
-		do 
-		{
-			printf("%s's turn(O): ", player1);
-			place_p(board);
-			if (check(board) > 0) break;
-			show_board(board);
-			printf("%s's turn(X): ", player2);
-			place_p(board);
-			show_board(board);
-		} while (check(board) == 0);
-		
+		playerSet();
+		gameStart();
+		gameResult();
 		getchar();
-		printf("Game over!\n");
-		printf("Player1(O): %s VS Player2(X): %s\n", player1, player2);
-		show_board(board);
-		if (status == 3) printf("Draw.\n");
-		else printf("Winner is %s!\n", (status == 1 ? player1 : player2));
 		printf("Want to play again? Press any key to continue.(Press Q if you want to stop.)\n");
 	} while (quit());
 	
@@ -47,6 +26,41 @@ int main()
 	return 0;
 }
 
+static void playerSet()
+{
+	do
+	{
+		if (temp) printf("Enter different nickname.\n");
+		printf("Enter Player1's name.\n< ");
+		s_get(player1, 50);
+		printf("Enter Player2's name.\n< ");
+		s_get(player2, 50);
+	} while (temp = !strcmp(player1, player2));
+	
+}
+static void gameStart();
+{
+	printf("Player1(O): %s VS Player2(X): %s\n", player1, player2);
+	show_board(board);
+	do 
+	{
+		printf("%s's turn(O): ", player1);
+		place_p(board);
+		if (check(board) > 0) break;
+		show_board(board);
+		printf("%s's turn(X): ", player2);	
+		place_p(board);
+		show_board(board);
+	} while (check(board) == 0);
+}
+static void gameResult()
+{
+	printf("Game over!\n");
+	printf("Player1(O): %s VS Player2(X): %s\n", player1, player2);
+	show_board(board);
+	if (status == 3) printf("Draw.\n");
+	else printf("Winner is %s!\n", (status == 1 ? player1 : player2));
+}
 static char * s_get(char * name, int max)
 {
 	int i = 0;
